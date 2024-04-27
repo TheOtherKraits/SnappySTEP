32ef55f9e7e326ac406b398105243b9ddccfe310
TheOtherKraits <42328643+TheOtherKraits@users.noreply.github.com>
2024-04-26 20:20:58 -0500

    Volume Names and Interface detection
    Made function that gets body names from STEP and started working on interface identification

--- src/readerFuncs.py
+++ src/readerFuncs.py
@@ -1,18 +1,8 @@
 import re
 
-def findNameLines(StepFile):
-    for line in StepFile:
-        if 'MANIFOLD_SOLID_BREP' in line:
-            yield line
-
-def getStepBodyNames(fullPath):
-    Names = []
-    with open(fullPath, 'r') as StepFile:
-        for line in findNameLines(StepFile):
-            Names.append = re.search(r"MANIFOLD_SOLID_BREP\(\'(.*?)\'\,\#"gm, StepFile)
 
 def regexStepBodyNames(fullPath): # Just regex all text?
     with open(fullPath, 'r') as StepFile:
-        bodyNames = re.findall(r"MANIFOLD_SOLID_BREP\(\'(.*?)\'\,\#"gm, StepFile)
+        bodyNames = re.findall(r"MANIFOLD_SOLID_BREP\(\'(.*?)\'\,\#", StepFile.read())
         return bodyNames
 
--- src/runSnappyStep.py
+++ src/runSnappyStep.py
@@ -2,6 +2,7 @@ import argparse
 import gmsh
 import xml.etree.ElementTree as ET
 import os
+from readerFuncs import *
 
 parser = argparse.ArgumentParser(description='Prepare STEP geometry for SnappyHexMesh using GMSH')
 parser.add_argument('filename', metavar='*.stp', type=str, nargs=1,help='STEP file to be processed')
@@ -26,20 +27,33 @@ gmsh.model.occ.synchronize()
 
 # Get Geometry Names
 print('Getting Names of Bodies and Surfaces')
-tree = ET.parse(os.path.join(geoPath[0],'NamedGeometry.xml')) # Read XML file that defines volumes and Named Faces
-root = tree.getroot()
-for Volume in root.findall("Volume"): # loop through all Volume entries
-    #print(Volume.get('name'))
-    #print(Volume.find('Tag').text)
-    gmsh.model.setEntityName(3,int(Volume.find("Tag").text),Volume.get('name')) # Adds names to gmsh entites
+volumes = gmsh.model.getEntities(3)
+VolNames = regexStepBodyNames(args.filename[0])
+print("Found Volumes: ",VolNames)
+for i, element in enumerate(VolNames): # loop through all Volume entries
+    gmsh.model.setEntityName(3,volumes[i][1],element) # Adds names to gmsh entites
 print('Volume names assigned')
+# tree = ET.parse(os.path.join(geoPath[0],'NamedGeometry.xml')) # Read XML file that defines volumes and Named Faces
+# root = tree.getroot()
+# for Volume in root.findall("Volume"): # loop through all Volume entries
+#     #print(Volume.get('name'))
+#     #print(Volume.find('Tag').text)
+#     gmsh.model.setEntityName(3,int(Volume.find("Tag").text),Volume.get('name')) # Adds names to gmsh entites
+
 
 # Loop through surfaces. Each surface may have more than one tag
 
 # Find shared boundaries
 print('Identifying contacts')
-volumes = gmsh.model.getEntities(3)
-geoBounds = gmsh.model.getBoundary(volumes)
+
+geoBounds = gmsh.model.getBoundary(volumes,False,False,False)
+Interfaces = {x for x in geoBounds if geoBounds.count(x) > 1} # Checks each index for more than one item the list, if so added to the set
+print(len(Interfaces), "contacting face(s) found.")
+# get volumes of interfaces
+interfaceVolPair = [] # List of dim, tag pairs of volume pairs of each interface
+for i, element in enumerate(Interfaces):
+    adj = gmsh.model.getAdjacencies(element[0],element[1])
+    interfaceVolPair.append([[2,adj[0][0]],[2,adj[0][1]]])
 
 
 gmsh.model.occ.synchronize()
