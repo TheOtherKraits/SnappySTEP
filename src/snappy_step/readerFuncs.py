cd10b3d80e1d584230bfd951820c1605bab70711
TheOtherKraits <42328643+TheOtherKraits@users.noreply.github.com>
2024-05-15 22:34:03 -0500

    Update readerFuncs.py
    Start writing function to write commands to modify snappyhexmeshdict

--- src/snappy_step/readerFuncs.py
+++ src/snappy_step/readerFuncs.py
@@ -6,3 +6,17 @@ def regexStepBodyNames(fullPath): # Just regex all text?
         bodyNames = re.findall(r"MANIFOLD_SOLID_BREP\(\'(.*?)\'\,\#", StepFile.read())
         return bodyNames
 
+def writeFoamDictionary(names: list[str],coordinate: list[float,float,float]) -> None:
+    commands = [] # use append to add to list
+    for i, element in enumerate(names):
+        print("commands for ", element)
+        # write foamDictinary command strings here for each object.
+        # Commands for first object
+        #if i == 1:
+
+
+        # Geometry Section
+
+        # Castellated Mesh Controls
+    # write file
+    fileName = "snappyStep.sh"
