95301ba453d2ae98eeac95d34a482ccded1cd277
TheOtherKraits <42328643+TheOtherKraits@users.noreply.github.com>
2025-06-08 21:09:44 -0500

    Function to write sHMD
    Wrote function to define sHMD. Still need to be tested and integrated.
    
    Wrote function to generate snappy step dictionary file defaults.

--- src/test_foamlib.py
+++ src/test_foamlib.py
@@ -1,4 +1,7 @@
 from snappy_step.readerFuncs import *
 
-write_block_mesh_dict([-1.1,-1.1,-1.1,1.1,1.1,1.1],0.25,0.25,0.25)
-
+# write_block_mesh_dict([-1.1,-1.1,-1.1,1.1,1.1,1.1],0.25,0.25,0.25)
+write_snappy_step_dict_template()
+old_sHMD = initialize_sHMD()
+write_sHMD_Geo("testGeo", ["region1", "region2a", "regionC"]);
+write_sHMD_Geo("testGeoInterface", [])
